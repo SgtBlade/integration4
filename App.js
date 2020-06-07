0b638a131799a5ae61b1ff976336b4094d2ce4ca
Miguel De Pelsmaeker <migueleken@hotmail.com>
2020-06-07 19:48:41 +0200

    Update of template

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/HomeScreen";
 import MapScreen from "./screens/MapScreen";
+const THEME_COLOR = 'red';
+const THEME_TEXT = 'white';
 
 export default function App() {
   const Stack = createStackNavigator();
 
   return (
     <>
+    <SafeAreaView style={styles.topSafeArea} />
+    <StatusBar barStyle="light-content" />
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}} >
-        <Stack.Screen name="home" component={HomeScreen}/>
+        <Stack.Screen name="home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  topSafeArea: {
+      flex: 0, 
+      backgroundColor: THEME_COLOR,
+      color: THEME_TEXT
+  }, 
+  bottomSafeArea: {
+      flex: 1, 
+      backgroundColor: THEME_COLOR,
+      color: THEME_TEXT
+  },
+});
