47178d3d042bf9092bdc6cb6408c2814a6519be5
Miguel De Pelsmaeker <migueleken@hotmail.com>
2020-06-15 21:11:01 +0200

    Fix attempt 2 infinite loops

--- src/App.js
+++ src/App.js
@@ -15,11 +15,6 @@ function App() {
 
   const [currentState, setCurrentState] = useState(STATE.UNAVAILABLE);
 
-  if(window.innerWidth === 1024 && window.innerHeight === 768) setCurrentState(STATE.AVAILABLE);
-  else if(window.innerHeight === 1024 && window.innerWidth === 768) setCurrentState(STATE.ROTATE);
-  else setCurrentState(STATE.UNAVAILABLE);
-  
-  
 
   //window.addEventListener('resize', handleResize)
   return useObserver (() => (
